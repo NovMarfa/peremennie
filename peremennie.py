5102cf7319246f45168487286ac2cd78d4e78bf8
NovMarfa <mn211195@gmail.com>
2024-11-24 18:58:45 +0300

    Add files via upload

---
+++ peremennie.py
@@ -0,0 +1,12 @@
+name_of_course0 = 'Курс: Python'
+task_count0 = 12
+hours_count0 = 1.5
+print (hours_count0 / task_count0)
+
+name_of_course = 'Курс: Python,'
+task_count = 12
+hours_count = 1.5
+lead_time = 0.125
+print ((name_of_course), 'всего задач:', task_count, ', затрачено часов:', hours_count, ', среднее время выполнения:', lead_time, 'часа')
+
+print ((name_of_course), 'всего задач:', task_count, ', затрачено часов:', hours_count, ', среднее время выполнения:', lead_time, 'часа', sep='')  # sep - чтобы убрать лишние пробелы
